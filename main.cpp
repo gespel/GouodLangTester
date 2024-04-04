86a29424a5695e43e5e88ccd5ac16dfe330c53b4
gespel <sten.heimbrodt@hotmail.de>
2024-04-04 15:14:45 +0200

    Feierabend

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 int main() {
     Lexer l;
     Interpreter i;
-    i.interpret(l.tokenize("function asd() { x2 = 2; return x2; } x3 = asd(); x = 7 + 7; x; 8;"));
-    i.printDebug();
+    i.interpret(l.tokenize("function asd() { x2 = 2; return x2; } x3 = asd(); x = 7 + 7; x; 8 + 7 + 3; asd();"));
+    //i.printDebug();
 
     return 0;
 }
