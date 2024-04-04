1ec7b88a15cb0ebb77ec7ab8a0a61ebd8a2812e9
gespel <sten.heimbrodt@hotmail.de>
2024-04-04 14:50:15 +0200

    added digit only expressions

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main() {
     Lexer l;
     Interpreter i;
-    i.interpret(l.tokenize("function asd() { x2 = 2; return x2; } x3 = asd(); x = 7 + 7; x;"));
+    i.interpret(l.tokenize("function asd() { x2 = 2; return x2; } x3 = asd(); x = 7 + 7; x; 8;"));
     i.printDebug();
 
     return 0;
