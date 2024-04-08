2d290fd6ef4bf96706171c9dcdaa27857216a50b
gespel <sten.heimbrodt@hotmail.de>
2024-04-08 08:13:48 +0200

    added file reading

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 
 int main() {
     GouodLang gl;
-    gl.interpretString("function asd() { x2 = 2; return x2; } x3 = asd(); x = 7 + 7; x; 8 + 7 + 3; asd();");
+    //gl.interpretString("function asd() { x2 = 2; return x2; } x3 = asd(); x = 7 + 7; x; 8 + 7 + 3; asd();");
+    gl.interpretFile("gouodlib/GouodLang/Examples/first.gouod");
 
     return 0;
 }
